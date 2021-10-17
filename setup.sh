80aa15d5a5bb0febda7f363c750aed920982ef7e
ssrlive <30760636+ssrlive@users.noreply.github.com>
2021-10-17 21:15:57 +0800

    ./runcgdb.sh issues

--- setup.sh
+++ setup.sh
@@ -4,9 +4,14 @@ export OSLAB_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null && pwd)
 debianSetup() {
     sudo dpkg --add-architecture i386
     sudo apt update
-    sudo apt-get install -y binutils
+    sudo apt-get install -y binutils curl
     sudo apt-get install -y binutils-common
     sudo apt-get install -y build-essential bin86 manpages-dev libc6-dev-i386 zlib1g:i386 libncurses5:i386 libsm-dev:i386 libx11-dev:i386 libxpm-dev:i386 libexpat1:i386
+
+    sudo apt-get install -y libncurses5
+    curl -L http://cz.archive.ubuntu.com/ubuntu/pool/main/r/readline/libreadline7_7.0-3_amd64.deb -o /tmp/libreadline.deb
+    sudo dpkg -i /tmp/libreadline.deb
+
 	cp ${OSLAB_PATH}/gcc-3.4-ubuntu.tar.gz /tmp
 	tar zxvf /tmp/gcc-3.4-ubuntu.tar.gz -C /tmp/
 	cd /tmp/gcc-3.4
